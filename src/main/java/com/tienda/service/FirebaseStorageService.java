c4f5f8ae9bd9fcc7bfd9ae6bf57c17c48b80486f
XPC <XPC@DESKTOP-I2RLDNC>
2025-06-21 13:15:44 -0600

    Finalizacion Semana06

---
+++ src/main/java/com/tienda/service/FirebaseStorageService.java
@@ -0,0 +1,24 @@
+
+package com.tienda.service;
+
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public interface FirebaseStorageService {
+
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+
+    //El BuketName es el <id_del_proyecto> + ".appspot.com"
+    final String BucketName = "92cb9.firebasestorage.app";
+
+    //Esta es la ruta básica de este proyecto Techshop
+    final String rutaSuperiorStorage = "techshop/spiedra90752";
+
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile = "firebase";
+    
+    //El nombre del archivo Json
+    final String archivoJsonFile = "techshop-92cb9-firebase-adminsdk-fbsvc-c71fc9c700.json";
+}
--- src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
+++ src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
@@ -6,23 +6,40 @@ import com.tienda.domain.Categoria;
 import com.tienda.service.CategoriaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-//Define la logica de los metodfos de la interfaz que Implementa  
-public class CategoriaServiceImpl implements CategoriaService{
-    
-    //Inyeccicon
+@Service
+public class CategoriaServiceImpl implements CategoriaService {
+
     @Autowired
-    private CategoriaDao categoriadao;
-            
-    @Override
-    public List<Categoria> getCategorias(boolean activos) {
-        List<Categoria> lista = categoriadao.findAll();
-        
-        //filtrar solo los activos
-        if (activos) {
-            lista.removeIf(cat -> !cat.isActivo());
-        }
+    private CategoriaDao categoriaDao;
 
+    @Override
+    @Transactional(readOnly = true)
+    public List<Categoria> getCategorias(boolean activos) {
+        var lista = categoriaDao.findAll();
+        if (activos) {
+            lista.removeIf(e -> !e.isActivo());
+        }
         return lista;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Categoria getCategoria(Categoria categoria) {
+        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Categoria categoria) {
+        categoriaDao.save(categoria);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Categoria categoria) {
+        categoriaDao.delete(categoria);
     }
 }
